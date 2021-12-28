9c08e89e20f832c968ca07dc8bb05509c4cbf61d
savannah030 <savannah030@naver.com>
2021-12-29 02:24:34 +0900

    [BOJ 1707] 이분그래프_주석추가

--- 2021-12-28_1707_이분그래프.py
+++ 2021-12-28_1707_이분그래프.py
@@ -1,6 +1,7 @@
 # 이분매칭: A 집단이 B 집단을 선택하는 방법에 대한 알고리즘
 # 이분매칭 bfs로도 풀수있음!
-# 주어진 정점들을 두 그룹으로 나누는 거기 때문에 9576 책나눠주기랑 다름!
+# 주어진 정점들을 두 그룹으로 나누는 거기 때문에(두 그룹 왔다갔다해야함) 9576 책나눠주기(그리디)랑 다름!
+### 이분그래프!=이분매칭 같은 개념 아님!!!! https://www.crocus.co.kr/499
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(200005)
@@ -8,12 +9,12 @@ sys.setrecursionlimit(200005)
 def bipartite(now,flag):
     group[now]=flag
     for nxt in graph[now]:
-        if not group[nxt]:
-            if not bipartite(nxt,-flag):
-                return False
-        elif group[nxt]==flag:
-            return False
-    return True
+        if not group[nxt]: # 아직 그룹이 정해지지 않은 nxt에 한해서만 확인
+            if not bipartite(nxt,-flag): # nxt로 bipartite함수 재귀호출. nxt부터가 이분그래프가 아니면
+                return False             # 이번 now부터의 그래프도 이분그래프가 아닌것이 당연
+        elif group[nxt]==flag: # group[nxt]!=0이어서 이미 방문한 노드는 맞는데, now랑 그룹이 같으면 
+            return False       # 이분그래프가 아님
+    return True # 모든 nxt가 이분그래프이면 now도 이분그래프가 맞음
 
 for K in range(int(input())):
     V,E = map(int,input().split()) # 정점,간선
@@ -23,10 +24,10 @@ for K in range(int(input())):
         graph[a-1].append(b-1) # 정점 0번부터 시작하도록 바꾸기
         graph[b-1].append(a-1)
         
-    group = [0]*V 
+    group = [0]*V # 0으로 초기화. bipartite함수에서 정점 지날때마다 1,-1,1,-1,.. 번갈아가며 값 할당(이분그래프 특징)
     for v in range(V): # 모든 점이 연결되어있지 않을 수도 있기 때문에 시작점만 dfs돌면 안됨(다 확인해줘야함)
-        if not group[v]: # 그룹이 아직 정해지지 않았으면
-            if not bipartite(v,1):
+        if not group[v]: # 그룹이 아직 정해지지 않았을때만 bipartite 함수실행
+            if not bipartite(v,1): 
                 print("NO")
                 break
     else:
