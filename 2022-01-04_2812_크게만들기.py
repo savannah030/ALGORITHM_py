ef214a36c308f9dd24c5683e1589b8b6e94c0be0
savannah030 <savannah030@naver.com>
2022-01-05 03:19:37 +0900

    [BOJ 2812] 크게만들기(그리디)_ver2(더간단)

--- 2022-01-04_2812_크게만들기.py
+++ 2022-01-04_2812_크게만들기.py
@@ -4,25 +4,17 @@ input = sys.stdin.readline
 
 N,K = map(int,input().split())
 li = list(map(int,input().rstrip()))
-stack = [li[0]]
-k = 0
-idx = N
-for n in range(1,N):
-    if k==K: 
-        idx = n
-        break
-    if li[n]<=stack[-1]: 
-        stack.append(li[n])
-    else: #### while문!!
-        while k<K and len(stack)>0 and li[n]>stack[-1]:
-            stack.pop()
-            k += 1
-        stack.append(li[n])
-    
-if k==K:
-    print(int(''.join(map(str,stack+li[idx:]))))
-else: # for문 끝까지 돌았음 -> 마지막 (K-k)개 숫자가 작아서 stack에 들어간것이므로 빼주기
-    print(int(''.join(map(str,stack[:len(stack)-(K-k)]))))
+k, stack = K, []
+
+for i in range(N):
+    while k > 0 and stack and stack[-1] < li[i]:
+        stack.pop()
+        k -= 1
+    stack.append(li[i])
+
+# for문 중간에 빠져나가지 않으니까 무조건 K개 빼주면 되는거임
+print(''.join(map(str,stack[:N-K])))
+
 
     
     
