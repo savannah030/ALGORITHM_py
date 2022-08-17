e1638baf36f897ff27db503bd0398b296e6550da
savannah030 <savannah030@naver.com>
2022-08-17 16:12:40 +0900

    8/17(수) 62050 크루스칼로 MST 만들기

--- 2022-08-17_프로그래머스_62050_지형이동.py
+++ 2022-08-17_프로그래머스_62050_지형이동.py
@@ -1,13 +1,15 @@
 # 모든 칸을 방문하기 위해 필요한 사다리 설치 비용의 최솟값
-# 2146 다리만들기처럼 인덱싱한다음 각 덩어리마다 height 가장 적은
-# 47분+20분
-# 시간초과는 그렇다 치는데 어디서 틀리는거지,,, 
+# 제일 큰 height 빼는 게 정답이 아님 !! (모두 이어진 상태가 아닐 수 있음)
+# 1. 2146 다리만들기처럼 인덱싱
+# 2. 인덱스 덩어리 w1,w2를 이어주는 간선 정보를 graph에 저장
+# 3. 크루스칼 알고리즘으로 MST 만들기
+# 47분+20분(디버깅)+11분(MST)
 from collections import deque
 def solution(land, height):
     N = len(land)
     dx = [0,0,1,-1]
     dy = [1,-1,0,0]
-    
+    # 1
     def indexing():
         q = deque()
         indices = [ [-1]*N for _ in range(N)]
@@ -29,9 +31,9 @@ def solution(land, height):
      
         return INDEX-1,indices
     
-    # (최대 인덱스)-1 개 놓으면 됨
+    # 2
     def setLadder(MAX_INDEX,indices):
-        ladders_matrix = [ [10001]*(MAX_INDEX+1) for _ in range(MAX_INDEX+1)]
+        graph = []
        
         for x in range(N):
             for y in range(N):
@@ -39,18 +41,36 @@ def solution(land, height):
                     nx,ny = x+dx[dir],y+dy[dir]
                     if nx<0 or nx>=N or ny<0 or ny>=N: continue
                     if indices[nx][ny]==indices[x][y]: continue
-                    elif ladders_matrix[indices[x][y]][indices[nx][ny]]>abs(land[nx][ny]-land[x][y]):
-                        ladders_matrix[indices[x][y]][indices[nx][ny]]=abs(land[nx][ny]-land[x][y])
-                        ladders_matrix[indices[nx][ny]][indices[x][y]]=abs(land[nx][ny]-land[x][y])
-    
-        cands = []
-        for st,info in enumerate(ladders_matrix[:-1]):
-            cands.extend([i for i in info[st+1:] if i<10001])
-                          
-        return cands
+                    graph.append((abs(land[nx][ny]-land[x][y]),indices[x][y],indices[nx][ny]))
+                    
+        return graph
             
     MAX_INDEX,indices = indexing()
-    cands = setLadder(MAX_INDEX,indices)
-    cands.sort()
-    return sum(cands[:MAX_INDEX])
+    graph = setLadder(MAX_INDEX,indices)
+    # 3. 크루스칼 알고리즘
+    # 3-1. 간선정보 비용에 따라 오름차순 정렬
+    graph = sorted(graph,key=lambda x:x[0])
+    parent = [x for x in range(MAX_INDEX+1)]
+    
+    def union_parent(x,y):
+        px = find_parent(x)
+        py = find_parent(y)
+        
+        if px<py:
+            parent[py]=px
+        else:
+            parent[px]=py
+    
+    def find_parent(x):
+        if parent[x]!=x:
+            parent[x]=find_parent(parent[x])
+        return parent[x]
+    
+    answer = 0
+    # 모든 간선에 대해 사이클 판별해야함(중간에 break x)
+    for (w,v1,v2) in graph:
+        if find_parent(v1)!=find_parent(v2): # 3-2. 사이클이 발생하지 않으면
+            union_parent(v1,v2)              # 3-3. 최소 신장 트리에 포함
+            answer += w
+    return answer
     
